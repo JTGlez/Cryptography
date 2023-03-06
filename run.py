1075da92d6f1f706b18f3486908195834264981f
JTGlez <jorjeluigi1@comunidad.unam.mx>
2023-03-06 15:59:25 -0600

    TestPro

--- run.py
+++ run.py
@@ -3,12 +3,13 @@ import fileinput
 suma = 0
 suma2 = 0
 
+def es_numero(str):
+    if str.replace('.', '').isdigit():
+        return int(str)
+    else:
+        return float(str)
+
 for f in fileinput.input():
-    suma = suma + int(f)
-
-print(suma)
-
-for g in fileinput.input():
-    suma2 = suma2 + float(g)
+    suma = suma + es_numero(f)
 
 print(suma)
